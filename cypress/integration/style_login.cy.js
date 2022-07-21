b93b7ae128b1e29a33ebece10597141fb477d8ca
anascutar <84850587+anascutar@users.noreply.github.com>
2022-07-21 15:33:58 +0300

    style_login.cy.js
    Modified

--- cypress/integration/style_login.cy.js
+++ cypress/integration/style_login.cy.js
@@ -9,9 +9,13 @@ describe('Login Test',() => {
     })
     it('Login page style',()=>{
         
-       // cy.get('.completed').should('have.css', 'text-decoration', 'line-through')
+       //am facut testarea pt desisgn
         cy.get('.sc-iqAclL').should('have.css', 'color', 'rgb(255, 255, 255)')
         cy.get('.sc-iqAclL').should('have.css', 'background-color', 'rgb(11, 37, 89)') 
+        cy.get('#email-address').should('have.css', 'padding', '16px')
+        cy.get('.sc-kmIPcE').should('have.css', 'font-size', '32px')
+        cy.get('.sc-kmIPcE').should('have.css', 'font-family', '"Open Sans", sans-serif')
+
     })
     
 })
